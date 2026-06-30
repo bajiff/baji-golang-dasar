3200d2ef6fe35287f5f733f7197991ce903f93b5
BajiLinux <mr.baji.smart@gmail.com>
2026-06-30 12:49:37 +0700

    Hari ke-4: Mesin Penggerak Dapur (Perulangan / Looping)

---
+++ latihan_3.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func latihanTiga(){
+	
+	var totalTagihan int = 7000
+	var tresholdUp = 100000 
+	var tresholdDown = 50000 
+	var statusPesanan = "ready"
+
+	// ? Logic
+	if totalTagihan > tresholdUp {
+		fmt.Printf("Dapet discount 10%% \n")
+	} else if totalTagihan > tresholdDown && totalTagihan < tresholdUp {
+		fmt.Printf("Dapet discount 5%% \n")
+	} else {
+		fmt.Print("Tidak dapat discount \n")
+	}
+	
+	switch statusPesanan {
+	case "coocking": fmt.Print("Koki sedang memasak dengan api besar!.\n")
+	case "ready": fmt.Print("Ting! Pesanan siap dihidangkan ke pelanggan.\n")
+	case "pending": fmt.Print("Pesanan sedang disiapkan, koki sedang cuci tangan.\n")
+	default: fmt.Print("Status pesanan tidak valid.\n") 
+	}
+}
