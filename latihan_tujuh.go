586547395c86dfcf2fac502d9253206700bb3ecd
BajiLinux <mr.baji.smart@gmail.com>
2026-06-30 20:01:26 +0700

    Lanjut ke Hari ke-8: Koki Mandiri & Kontrak Kerja (Methods & Interfaces)

---
+++ latihan_tujuh.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type Pegawai struct {
+	Nama      string
+	Energi    int
+	Jabatan   string
+	Gaji      int
+	IsActive  bool
+	IsMarried bool
+	Tunjangan int
+	Usia      int
+	Skills    []string
+}
+
+func kerjaLembur(p *Pegawai) int {
+	p.Energi = p.Energi - 30
+	fmt.Printf("Pegawai %s sisa Energi %d\n", p.Nama, p.Energi)
+	return p.Energi
+}
+
+func latihanTujuh() {
+	pegawaiBaru := Pegawai{
+		Nama:      "Baji",
+		Energi:    100,
+		Jabatan:   "CEO",
+		Gaji:      50000000,
+		IsActive:  true,
+		IsMarried: false,
+		Tunjangan: 40000000,
+		Usia:      21,
+		Skills:    []string{"Javascript", "ReactJS", "Tailwindcss", "Axios", "Golang"},
+  }
+	fmt.Println(&pegawaiBaru)
+	kerjaLembur(&pegawaiBaru)
+	fmt.Println(&pegawaiBaru.Energi)
+
+}
--- main.go
+++ main.go
@@ -7,5 +7,6 @@ func main() {
 	// latihanTiga()
 	// latihanEmpat()
 	// latihanLima()
-	latihanEnam()
+	// latihanEnam()
+	latihanTujuh()
 }
