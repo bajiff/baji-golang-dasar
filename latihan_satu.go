73bef95b1f641a196625c23a8deb4e7d703dd30f
BajiLinux <mr.baji.smart@gmail.com>
2026-06-30 11:45:01 +0700

    Hari ke-2: Penjaga Kualitas Bahan Baku (Tipe Data & Operasi)

--- latihan_satu.go
+++ latihan_satu.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hari-1")
+	const restaurantName string = "Baji Kempong Restaurant"
+	var since int = 2000
+	var specialMenu string = "Ketoprak"
+
+	fmt.Printf("Welcome to %s yang lahir sejak %d dengan menu spesial %s\n", restaurantName,since,specialMenu)
 }
